6d5756a2ebfea62f785b24bcab13dec13b84a34c
AisAif <Isbahrulfahni@gmail.com>
2024-06-13 22:07:05 +0700

    chore: add global error handler

---
+++ src/middleware/error_handler.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"errors"
+
+	"github.com/AisAif/recipe-management-rest-api/src/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/iancoleman/strcase"
+	"github.com/spf13/viper"
+)
+
+func GlobalErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		err := c.Errors.Last()
+		if err != nil {
+			switch err.Err.(type) {
+			case validator.ValidationErrors:
+				var ve validator.ValidationErrors
+				if errors.As(err, &ve) {
+					out := make([]utils.ValidationError, len(ve))
+					for i, fe := range ve {
+						out[i] = utils.ValidationError{
+							Field:   strcase.ToSnake(fe.Field()),
+							Message: utils.MessageForTag(fe.Tag()),
+						}
+					}
+					c.JSON(400, gin.H{"errors": out})
+				}
+
+				return
+			default:
+				if viper.GetString("GIN_MODE") == "debug" {
+					c.JSON(500, gin.H{
+						"message": "Internal server error.",
+						"error":   err,
+					})
+				} else {
+					c.JSON(500, gin.H{
+						"message": "Internal server error.",
+					})
+				}
+			}
+
+			c.Abort()
+		}
+	}
+}
---
+++ src/routes/init.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"github.com/AisAif/recipe-management-rest-api/src/config"
+	"github.com/AisAif/recipe-management-rest-api/src/middleware"
+	"github.com/AisAif/recipe-management-rest-api/src/models"
+	"github.com/AisAif/recipe-management-rest-api/src/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func InitRouter() *gin.Engine {
+	if err := config.Init(); err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+
+	utils.InitLog()
+	models.InitDB()
+	r := gin.Default()
+
+	r.Use(middleware.GlobalErrorHandler())
+
+	return r
+}
