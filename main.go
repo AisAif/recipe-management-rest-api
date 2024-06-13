d9b2611b1cc3f2a6bbb8846fa0fe9cf606507230
AisAif <Isbahrulfahni@gmail.com>
2024-06-13 22:20:09 +0700

    refactor: just router in main file

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"github.com/AisAif/recipe-management-rest-api/src/config"
-	"github.com/AisAif/recipe-management-rest-api/src/utils"
-	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
+	"github.com/AisAif/recipe-management-rest-api/src/routes"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	if err := config.Init(); err != nil {
-		log.Fatal().Msg(err.Error())
-	}
+	router := routes.InitRouter()
 
-	utils.InitLog()
-
-	r := gin.Default()
-
-	r.Run(":" + viper.GetString("port"))
+	router.Run(":" + viper.GetString("port"))
 }
