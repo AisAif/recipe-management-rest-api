768ac83ed227748498c6ef8fca487ec2be8e8710
AisAif <Isbahrulfahni@gmail.com>
2024-06-15 01:23:43 +0700

    chore: change password byte size

--- src/models/user.go
+++ src/models/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	Username  string `gorm:"primaryKey"`
-	Name      string
-	Password  string
+	Name      string `gorm:"size:255"`
+	Password  string `gorm:"size:60"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
