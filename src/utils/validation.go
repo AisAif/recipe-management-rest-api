9974c7f8073861e824ce95d87ad8944a7610ce2a
AisAif <Isbahrulfahni@gmail.com>
2024-07-15 23:40:32 +0700

    chore: update custom validation for file

--- src/utils/validation.go
+++ src/utils/validation.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"mime/multipart"
+	"strconv"
+	"strings"
+
 	"github.com/AisAif/recipe-management-rest-api/src/models"
 	"github.com/go-playground/validator/v10"
 )
@@ -13,6 +17,8 @@ type ValidationError struct {
 func InitValidator() *validator.Validate {
 	validate := validator.New(validator.WithRequiredStructEnabled())
 
+	// custom validations
+
 	validate.RegisterValidation("username_exists", func(fl validator.FieldLevel) bool {
 		username := fl.Field().String()
 
@@ -20,6 +26,45 @@ func InitValidator() *validator.Validate {
 		models.DB.First(&user, "username = ?", username)
 
 		return user.Username == ""
+	})
+
+	validate.RegisterValidation("file_type", func(fl validator.FieldLevel) bool {
+		fileHeader, ok := fl.Field().Interface().(multipart.FileHeader)
+		if !ok {
+			return false
+		}
+
+		fileType := fileHeader.Header.Get("Content-Type")
+		allowedTypes := strings.Split(fl.Param(), ":")
+
+		for _, t := range allowedTypes {
+			t = strings.TrimSpace(t)
+			if strings.HasSuffix(t, "/*") {
+				if strings.HasPrefix(fileType, strings.TrimSuffix(t, "/*")) {
+					return true
+				}
+			} else {
+				if t == fileType {
+					return true
+				}
+			}
+		}
+
+		return false
+	})
+
+	validate.RegisterValidation("max_size", func(fl validator.FieldLevel) bool {
+		maxFileSize, err := strconv.Atoi(fl.Param())
+		if err != nil {
+			return false
+		}
+
+		fileHeader, ok := fl.Field().Interface().(multipart.FileHeader)
+		if !ok {
+			return false
+		}
+
+		return int(fileHeader.Size) <= maxFileSize*1024
 	})
 
 	return validate
@@ -35,6 +80,10 @@ func MessageForTag(tag string, param string) string {
 		return "MAX:" + param
 	case "username_exists":
 		return "USERNAME_EXISTS"
+	case "file_type":
+		return "FILE_TYPE:" + param
+	case "max_size":
+		return "MAX_SIZE:" + param
 	default:
 		return "UNKNOWN"
 	}
