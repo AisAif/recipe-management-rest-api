873b81fcbf2c7b3dcea40ae871117b20627963df
AisAif <Isbahrulfahni@gmail.com>
2024-07-17 00:31:19 +0700

    chore: add page info field in response body

--- src/http/resources/resource.go
+++ src/http/resources/resource.go
@@ -1,7 +1,10 @@
 package resources
 
+import "github.com/rosberry/go-pagination"
+
 type Resource[T any] struct {
-	Data    T      `json:"data"`
-	Message string `json:"message"`
-	Errors  any    `json:"errors"`
+	Data     T                   `json:"data"`
+	Message  string              `json:"message"`
+	Errors   any                 `json:"errors"`
+	PageInfo pagination.PageInfo `json:"page_info"`
 }
